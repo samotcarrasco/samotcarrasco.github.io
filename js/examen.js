660faa0b4a085a31c7eb492ce961e085add56087
samot <samotcarrasco@gmail.com>
2023-01-22 21:41:32 +0100

    añadiendo hijos

--- js/examen.js
+++ js/examen.js
@@ -11,97 +11,156 @@ xhttp.send();
 
 
 xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    let datos = JSON.parse(this.responseText);
-    console.log(datos);
+    if (this.readyState == 4 && this.status == 200) {
+        let datos = JSON.parse(this.responseText);
+        console.log(datos);
+        //resto de código
+
+        //guardamos en un array numeros aletorios sin repetir
+        let randoms = [];
+        while (randoms.length < 4) {
+            let random = Math.floor(Math.random() * 4) + 1;
+            if (randoms.indexOf(random) == -1) {
+                randoms.push(random);
+            }
+        }
 
 
-    //guardamos en un array numeros aletorios sin repetir
-    let randoms = [];
-    while (randoms.length < 4) {
-      let random = Math.floor(Math.random() * 4) + 1;
-      if (randoms.indexOf(random) == -1) {
-        randoms.push(random);
-      }
+        console.log("order aleotiro de lectura de preguntas: " + randoms);
+        let contador = 1;
+        let preguntasGeneradas = 0;
+        let asignadas = [];
+        do {
+
+            let aleatorio = randoms[contador - 1];
+
+            console.log("---------------" + aleatorio);
+            let pregunta = "p" + contador;
+            console.log("Generando pregunta " + pregunta + " leida pregunta de tipo: " + datos.preguntas[aleatorio - 1].tipo);
+            //los id de las preguntas son: p1, p2, p3 .......
+            console.log("ID PREGUNTA LEIDA: " + datos.preguntas[aleatorio - 1].id);
+            console.log("CONTADOR: " + contador)
+
+            let mismoTipo = false;
+            let tipoPregunta = "";
+            // const classNames = document.getElementById(pregunta).classList;
+
+
+             let aleatorio2 = aleatorio;
+            // do {
+            //     let id = datos.preguntas[aleatorio2 - 1].id;
+            //     classNames.forEach(name => {
+
+            //         tipoPregunta = datos.preguntas[aleatorio2 - 1].tipo;
+            //         if (name === tipoPregunta) {
+            //             console.log("Clase coincide " + pregunta + " id=" + id);
+            //             mismoTipo = true;
+            //         }
+            //         //si no son del mismo tipo, probamos con la siguiente pregunta.
+            //     })
+            //     if (!mismoTipo) {
+            //         aleatorio2++;
+            //         console.log("clase no coincide, vamos a probar con la siguiente pregunta");
+            //     }
+            // } while (!mismoTipo);
+
+
+            // if (mismoTipo) {
+
+
+                //generamos el objeto Pregunta
+                const id = datos.preguntas[aleatorio2 - 1].id;
+                const enunciado = datos.preguntas[aleatorio2 - 1].enunciado;
+                const opciones = [datos.preguntas[aleatorio2 - 1].opciones.opc1,
+                datos.preguntas[aleatorio2 - 1].opciones.opc2,
+                datos.preguntas[aleatorio2 - 1].opciones.opc3,
+                datos.preguntas[aleatorio2 - 1].opciones.opc4];
+                let tipo = datos.preguntas[aleatorio2 - 1].tipo;
+                let imagen = datos.preguntas[aleatorio2 - 1].imagen;
+                let correcta = datos.preguntas[aleatorio2 - 1].correcta;
+
+                const preguntaObj = new Pregunta(id, enunciado, opciones, tipo, imagen, correcta)
+
+                console.log(" Generada pregunta" + preguntaObj.id);
+
+                anadirPreguntaHTML(preguntaObj,contador);
+
+            //     console.log("modificando DOM " + pregunta);
+            //     if (tipoPregunta === "imagen") {
+            //         let idImagen = "imagen" + pregunta;
+            //         let elemento = document.getElementById(idImagen);
+            //         console.log(datos.preguntas[aleatorio2 - 1].imagen);
+            //         elemento.src = datos.preguntas[aleatorio2 - 1].imagen;
+            //     }
+            //   //  document.getElementById(pregunta).innerHTML = contador + ".- " + datos.preguntas[aleatorio2 - 1].enunciado;
+                contador++;
+                // for (let i = 1; i <= 4; i++) {
+                //     let opcion = "opc" + i;
+                //     let opcion2 = pregunta + opcion;
+                //     document.getElementById(opcion2).innerHTML = datos.preguntas[aleatorio2 - 1].opciones[opcion]; //ojoo --> hay que quitar el punto y poner la variable entre corchetes
+                // }
+                preguntasGeneradas++;
+            //}
+
+        } while (preguntasGeneradas < 4);
+
     }
-
-
-    console.log("order aleotiro de lectura de preguntas: " + randoms);
-    let contador = 1;
-    let preguntasGeneradas = 0;
-    let asignadas = [];
-    do {
-
-      let aleatorio = randoms[contador - 1];
-
-      console.log("---------------" + aleatorio);
-      let pregunta = "p" + contador;
-      console.log("Generando pregunta " + pregunta + " leida pregunta de tipo: " + datos.preguntas[aleatorio - 1].tipo);
-      //los id de las preguntas son: p1, p2, p3 .......
-      console.log("ID PREGUNTA LEIDA: " + datos.preguntas[aleatorio - 1].id);
-      console.log("CONTADOR: " + contador)
-
-      let mismoTipo = false;
-      let tipoPregunta="";
-      const classNames = document.getElementById(pregunta).classList;
-
-      
-      let aleatorio2=aleatorio;
-      do {
-      //   console.log("----------" + classNames);
-       //  console.log("-----------" + datos.preguntas[aleatorio2 - 1].tipo);
-        // console.log("ale2" + aleatorio2);
-        // console.log(datos.preguntas[aleatorio2 - 1].tipo);
-        let id = datos.preguntas[aleatorio2 - 1].id;
-         classNames.forEach(name => {
-          //console.log(name + "VVVSSS");
-          //console.log("ale2 " + aleatorio2);
-          //console.log(datos.preguntas[aleatorio2 - 1].tipo);
-          
-          tipoPregunta = datos.preguntas[aleatorio2 - 1].tipo;
-          //console.log(name +"**"+tipoPregunta);
-          if (name === tipoPregunta) {
-            console.log("Clase coincide " + pregunta + " id=" + id);
-            mismoTipo = true;
-          }
-          //si no son del mismo tipo, probamos con la siguiente pregunta.
-        })
-        if (!mismoTipo) {
-          aleatorio2++;
-          console.log("clase no coincide, vamos a probar con la siguiente pregunta");
-        }
-      } while (!mismoTipo);
-    
-
-      if (mismoTipo) {
-        console.log("modificando DOM "+pregunta);
-        if (tipoPregunta==="imagen") {
-          let idImagen = "imagen" + pregunta;
-          let elemento = document.getElementById(idImagen);
-          console.log(datos.preguntas[aleatorio2-1].imagen);
-          elemento.src= datos.preguntas[aleatorio2-1].imagen;
-        }
-        document.getElementById(pregunta).innerHTML = contador + ".- " + datos.preguntas[aleatorio2 - 1].enunciado;
-        contador++;
-        for (let i = 1; i <= 4; i++) {
-          let opcion = "opc" + i;
-          let opcion2 = pregunta + opcion;
-          document.getElementById(opcion2).innerHTML = datos.preguntas[aleatorio2 - 1][opcion]; //ojoo --> hay que quitar el punto y poner la variable entre corchetes
-        }
-        preguntasGeneradas++;
-      }
-     
-    } while (preguntasGeneradas < 3);
-    let imagenp = document.getElementById("imagenp4");
-    console.log(datos.preguntas[6].imagen);
-    imagenp.src= datos.preguntas[6].imagen;
-
-  }
 
 }
 
+//constructor de los objetos Pregunta
+function Pregunta(id, enunciado, opciones = [], tipo, imagen, correcta) {
+    this.id = id;
+    this.enunciado = enunciado;
+    this.opciones = opciones;
+    this.tipo = tipo;
+    this.imagen = this.imagen;
+    this.correcta = this.correcta;
+}
 
+function anadirPreguntaHTML(preguntaObj,contador) {
+    let codigoHtml ="";
+    switch (preguntaObj.tipo) {
+        case "larga":
+            codigoHtml ="<form class=\"row g-2\"> " +
+             " <article class=\"col-12\"> " +
+             "<p class=\"larga fw-bold mt-3\" id=\"p"+ contador + "\"> "+contador +".-"+preguntaObj.enunciado +  "</p>" +
+               " <div class=\"row\"> "+
+                    "<input type=\"radio\" name=\"box\" id=\"p"+contador+"r1" +"\"> " +
+                    "<input type=\"radio\" name=\"box\" id=\"p"+contador+"r2" +"\"> " +
+                    "<input type=\"radio\" name=\"box\" id=\"p"+contador+"r3" +"\"> " +
+                    "<input type=\"radio\" name=\"box\" id=\"p"+contador+"r4" +"\"> " +
+                    "<label for=\"p"+contador+"r1\" class=\"box p"+contador +"r1\"> " +
+                     "   <div class=\"course\"> <span class=\"circle\"></span> <span id=\"p" +contador +"opc1"+"\">" +preguntaObj.opciones[0] +"</span> </div> </label>" +
+                     "<label for=\"p"+contador+"r2\" class=\"box p"+contador +"r2\"> " +
+                     "   <div class=\"course\"> <span class=\"circle\"></span> <span id=\"p" +contador +"opc2"+"\">" +preguntaObj.opciones[1] +"</span> </div> </label>" +
+                     "<label for=\"p"+contador+"r3\" class=\"box p"+contador +"r3\"> " +
+                     "   <div class=\"course\"> <span class=\"circle\"></span> <span id=\"p" +contador +"opc3"+"\">" +preguntaObj.opciones[2] +"</span> </div> </label>" +
+                     "<label for=\"p"+contador+"r4\" class=\"box p"+contador +"r4\"> " +
+                     "   <div class=\"course\"> <span class=\"circle\"></span> <span id=\"p" +contador +"opc4"+"\">" +preguntaObj.opciones[3] +"</span> </div> </label>" +
+                     "</div>" +
+            "</article>" +
+        "</form>"
+            break;
+        case "corta":
+            console.log("ccccccorta");
+            break;
+        case "imagen":
+            console.log("llllllarga");
+            break;
+    }
 
+    console.log(codigoHtml);
+    var padre = document.getElementById("padrePreguntas");
+    var nuevoHijo = document.createElement("form");
+    nuevoHijo.classList.add("row","g-2");
+    nuevoHijo.innerHTML = codigoHtml;
+    
+    // Añade el nuevo nodo hijo al nodo padre
+    padre.appendChild(nuevoHijo);
+
+    console.log(nuevoHijo);
+}
 
 
 
