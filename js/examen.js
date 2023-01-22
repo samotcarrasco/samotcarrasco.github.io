dd1770ffc687bce162bd45bc07ee9ee025546eed
samot <samotcarrasco@gmail.com>
2023-01-22 04:03:08 +0100

    4

--- js/examen.js
+++ js/examen.js
@@ -25,26 +25,52 @@ xhttp.onreadystatechange = function () {
       }
     }
 
-    for (let i = 1; i <= 3; i++) {
-      //let aleatorio = Math.floor(Math.random() * 3)+1;
-      //console.log(aleatorio);
-      let aleatorio = randoms[i - 1];
+    console.log(randoms);
+    let contador = 1;
+    let preguntasGeneradas = 0;
+    do {
+
+      let aleatorio = randoms[contador - 1];
+      
       console.log("---------------" + aleatorio);
-      let pregunta = "p" + i;
+      let pregunta = "p" + contador;
       console.log(pregunta);
       //los id de las preguntas son: p1, p2, p3 .......
-      console.log(i + ".... " + datos.preguntas[aleatorio - 1].enunciado);
-      document.getElementById(pregunta).innerHTML = i + ".- " + datos.preguntas[aleatorio - 1].enunciado;
+      console.log(contador + ".... " + datos.preguntas[aleatorio - 1].enunciado);
 
+      let mismoTipo = false;
 
-      for (let i = 1; i <= 4; i++) {
-        let opcion = "opc" + i;
-        let opcion2 = pregunta + opcion;
-        document.getElementById(opcion2).innerHTML = datos.preguntas[aleatorio - 1][opcion]; //ojoo --> hay que quitar el punto y poner la variable entre corchetes
+      const classNames = document.getElementById(pregunta).classList;
+
+      console.log(classNames + "VVVSSS");
+      console.log(datos.preguntas[aleatorio - 1].tipo);
+
+      classNames.forEach(name => {
+        if (name == datos.preguntas[aleatorio - 1].tipo) {
+          mismoTipo = true;
+          console.log("MIsMO TIPOO");
+        }
+      })
+
+      if (mismoTipo) {
+        document.getElementById(pregunta).innerHTML = contador + ".- " + datos.preguntas[aleatorio - 1].enunciado;
+        contador++;
+        for (let i = 1; i <= 4; i++) {
+          let opcion = "opc" + i;
+          let opcion2 = pregunta + opcion;
+          document.getElementById(opcion2).innerHTML = datos.preguntas[aleatorio - 1][opcion]; //ojoo --> hay que quitar el punto y poner la variable entre corchetes
+        }
+        preguntasGeneradas++;
       }
-    }
+      else {
+        console.log("NO MISOOMO TIPOO");
+        contador++;
+      }
+
+    } while (preguntasGeneradas < 3);
   }
 }
+
 
 
 
